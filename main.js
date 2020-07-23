e86ed83b1f3836d306bdb7bdec388a063e59eef4
aorange <1041249649@qq.com>
2020-07-23 13:33:13 +0800

    程序员A修改main.js

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 function main(){
     var a = 100
     var b = 200
+    console.log(1000,'by gitA')
 }
 
 export  function auth(){
     return localStorage.getItem("token");
 }
 
+class GitAPerson {
+    constructor(){
+        this.init()
+    }
+    init(){
+        this.a = 100
+        console.log('xixixixi')
+    }
+    
+}
+
+new GitAPerson()
