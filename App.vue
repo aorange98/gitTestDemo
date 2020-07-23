117a6804b968e1e3896cf6cf9e58e9659b0f4b16
aorange <1041249649@qq.com>
2020-07-23 12:43:13 +0800

    初始化

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+<div class='app'>
+    <h2>啦啦啦啦啦啦啦</h2>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            a:10
+        }
+    },
+    components: {},
+    methods: {}
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
---
+++ main.js
@@ -0,0 +1,9 @@
+function main(){
+    var a = 100
+    var b = 200
+}
+
+export  function auth(){
+    return localStorage.getItem("token");
+}
+
