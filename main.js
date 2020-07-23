a709e663b571e18704510653b468a087204d77be
aorange <1041249649@qq.com>
2020-07-23 13:04:45 +0800

    程序员B新增navbar 和修改其他

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 function main(){
     var a = 100
     var b = 200
+    var gitB = '我是gitB'
 }
 
 export  function auth(){
     return localStorage.getItem("token");
 }
 
+var gitBCode = function(){
+    console.log('我是gitBCode')
+}
---
+++ navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='navbar'>
+      我是navbar
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  data(){
+    return {}
+  },
+  components: {},
+  methods: {}
+}
+</script>
+<style lang='less' scoped>
+</style>
